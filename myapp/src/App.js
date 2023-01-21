893974d9ed45de6ea837dfc8feadce6dcee185c9
Manivannan S <104342804+Manivannan-SenthamaraiKannan@users.noreply.github.com>
2023-01-21 12:17:05 +0530

    Header and Page Details

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,11 @@
 import Footer from './Components/Footer';
+import Header from './Components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Footer/>
     </div>
   );

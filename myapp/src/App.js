83794e1dc06c411f56965f7bac42b6278e334d96
Manivannan S <sharvamanivannan@gmail.com>
2023-01-24 04:54:57 +0000

    Changes in header and Cards

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,15 @@
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import PageHeader from './Components/PageHeader';
+import Cards from './Components/Cards'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <PageHeader/>
+      <Cards/>
       <Footer/>
     </div>
   );

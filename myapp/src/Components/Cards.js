f9e834856d2d9ac5438b7dd839b72f6d6c4379bd
Manivannan S <sharvamanivannan@gmail.com>
2023-01-24 06:06:20 +0000

    All components are implemented.

--- myapp/src/Components/Cards.js
+++ myapp/src/Components/Cards.js
@@ -26,16 +26,11 @@ const Cards = () => {
                     </div>
                     <div class="col mb-5">
                         <div class="card h-100">
-
                             <div class="badge bg-dark text-white position-absolute" style={onsale}>Sale</div>
-
                             <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-
                             <div class="card-body p-4">
                                 <div class="text-center">
-
                                     <h5 class="fw-bolder">Special Item</h5>
-
                                     <div class="d-flex justify-content-center small text-warning mb-2">
                                         <div class="bi-star-fill"></div>
                                         <div class="bi-star-fill"></div>
@@ -43,12 +38,10 @@ const Cards = () => {
                                         <div class="bi-star-fill"></div>
                                         <div class="bi-star-fill"></div>
                                     </div>
-
                                     <span class="text-muted text-decoration-line-through">$20.00</span>
                                     $18.00
                                 </div>
                             </div>
-
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                                 <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#!">Add to cart</a></div>
                             </div>
